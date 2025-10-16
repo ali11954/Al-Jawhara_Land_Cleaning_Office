2caf54dea8abc74dabd76b7a3b356c01f3e8915b
ali_mubark <alimubark440@gmail.com>
2025-10-16 16:28:20 +0300

    Add application variable for Gunicorn

--- wsgi.py
+++ wsgi.py
@@ -14,6 +14,9 @@ with app.app_context():
 
     print("✅ تم تهيئة التطبيق بنجاح")
 
+# هذا هو المتغير الذي يبحث عنه Gunicorn
+application = app
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
